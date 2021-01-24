ef4fbc5ac3a524f9982eb00978edc75c56dc9eff
Elvin <elvinrasul@gmail.com>
2021-01-24 14:55:28 -0500

    sets update

---
+++ lecture1/sets.py
@@ -0,0 +1,17 @@
+# lets create empty set
+
+s = set()
+
+# add elements to set
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(6)
+s.remove(6)
+
+print(s)
+
+print(f"The set has {len(s)} elements.")
