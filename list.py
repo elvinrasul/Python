9b900ac01d6b6ebca9a2a0da819337ba32b0cdc0
Elvin <elvinrasul@gmail.com>
2021-01-24 14:03:04 -0500

    list

---
+++ list.py
@@ -0,0 +1,23 @@
+# Define a list of names
+
+# 1st
+
+names = ["Harry", "Ron", "Ahmed", "Anna"]
+
+#print(names[0])
+
+#print(names)
+
+#----------------
+# we can use (.append) it mean add new name for example
+# we gonna use sort method. what is the sort method for example 
+
+
+names.append("Sasha")
+names.sort()
+
+print(names)
+ 
+
+
+
